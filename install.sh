e0fc1d61483f69b75a46fa7ad602e7d1cad5c125
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2023-11-02 07:15:55 +0000

    adding application install to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 # quick start symlinking configs
 # I'll make this better in the future 
 
+
+sudo apt update 
+sudo apt install -y $applicationList rofi qtile picom
+
+
 for d in .config/* ; do
     echo "ln -s $PWD/$d ~/.config/$d"
 done
